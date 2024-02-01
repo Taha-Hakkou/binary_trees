f12b0fe3c55d85104ab8075c17c7b569a9ff7d96
Taha-Hakkou <hakkoutaha16@gmail.com>
2024-02-01 17:58:54 +0100

    binary_trees is_heap done

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -9,13 +9,12 @@ int binary_tree_is_heap(const binary_tree_t *tree)
 {
 	binary_tree_t **nodes;
 	int size = 1, i, j;
-	int perfect = 1;
 
 	if (tree == NULL)
 		return (0);
 	nodes = malloc(sizeof(binary_tree_t *));
 	*nodes = (binary_tree_t *)tree;
-	while (perfect)
+	while (1)
 	{
 		size *= 2;
 		nodes = realloc(nodes, size * sizeof(binary_tree_t *));
@@ -28,17 +27,21 @@ int binary_tree_is_heap(const binary_tree_t *tree)
 			nodes[i * 2 + 1] = nodes[i]->right;
 			nodes[i * 2] = nodes[i]->left;
 		}
-		for (i = 0; i < size; i++)
-			if (nodes[i] == NULL)
-				break;
+		for (i = 0; i < size && nodes[i]; i++)
+			continue;
 		if (i < size)
-			perfect = 0;
+			break;
 	}
 	for (j = i + 1; j < size; j++)
 		if (nodes[j])
 			return (0);
 	for (j = 0; j < i; j++)
-		if (nodes[j]->left || nodes[j]->right)
+		if ((j > i && nodes[j]) ||
+				(j < i && (nodes[j]->left || nodes[j]->right)))
+		{
+			free(nodes);
 			return (0);
+		}
+	free(nodes);
 	return (1);
 }
