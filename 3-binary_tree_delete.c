4d4ebf67287b568d963c29d31ec107d21fafdc3a
Taha-Hakkou <hakkoutaha16@gmail.com>
2024-02-01 18:13:03 +0100

    binary_trees del done

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,10 +8,9 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree->right)
-		binary_tree_delete(tree->right);
-	if (tree->left)
-		binary_tree_delete(tree->left);
-	if (tree)
-		free(tree);
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
 }
